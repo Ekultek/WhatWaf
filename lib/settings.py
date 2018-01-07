20daae111564578325b0ec0b7f34d78b9da94aca
ekultek <thomas.perkins23@icloud.com>
2018-01-06 22:02:41 -0600

    bumped the version number

--- lib/settings.py
+++ lib/settings.py
@@ -11,7 +11,7 @@ from bs4 import BeautifulSoup
 import lib.formatter
 
 # version number <major>.<minor>.<commit>
-VERSION = "0.1.12"
+VERSION = "0.1.13"
 
 # version string
 VERSION_TYPE = "(#dev)" if VERSION.count(".") > 1 else "(#stable)"
