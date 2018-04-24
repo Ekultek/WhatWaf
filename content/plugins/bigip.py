c137107d2d4e8f251caa48349d213b57750f049a
ekultek <thomas.perkins23@icloud.com>
2018-04-24 14:14:32 -0500

    added a new regex to bigip detection

--- content/plugins/bigip.py
+++ content/plugins/bigip.py
@@ -10,10 +10,13 @@ def detect(content, **kwargs):
     headers = kwargs.get("headers", None)
     detection_schema = (
         re.compile(r"\ATS\w{4,}=", re.I), re.compile(r"biG]gip|bipserver", re.I),
-        re.compile(r"\AF5\Z", re.I)
+        re.compile(r"\AF5\Z", re.I),
+        re.compile(r"^TS[a-zA-Z0-9]{3,8}=", re.I)
     )
     for detection in detection_schema:
         if detection.search(headers.get(HTTP_HEADER.SERVER, "")) is not None:
             return True
         if detection.search(headers.get(HTTP_HEADER.SET_COOKIE, "")) is not None:
+            return True
+        if detection.search(headers.get(HTTP_HEADER.COOKIE, "")) is not None:
             return True
