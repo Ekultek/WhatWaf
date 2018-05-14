fc74e9e57907829faa665f4c46c0af966b109bc4
ekultek <thomas.perkins23@icloud.com>
2018-05-14 13:48:00 -0500

    minor wording update

--- lib/cmd.py
+++ lib/cmd.py
@@ -50,7 +50,7 @@ class WhatWafParser(ArgumentParser):
         output_opts.add_argument("-F", "--format", action="store_true", dest="formatOutput",
                                  help="Format the output into a dict and display it")
         output_opts.add_argument("-J", "--json", action="store_true", dest="sendToJSON",
-                                 help="Send the output to a JSON format")
+                                 help="Send the output to a JSON file")
         output_opts.add_argument("-Y", "--yaml", action="store_true", dest="sendToYAML",
                                  help="Send the output to a YAML file")
         output_opts.add_argument("-C", "--csv", action="store_true", dest="sendToCSV",
