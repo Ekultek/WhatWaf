3b8c660976a0cc67ecf13ba5c6a7672d27c75289
ekultek <thomas.perkins23@icloud.com>
2018-07-20 08:36:02 -0500

    update for configserver firewall (issue #93)

--- content/plugins/configserver.py
+++ content/plugins/configserver.py
@@ -6,7 +6,13 @@ __product__ = "ConfigServer SPI WAF (ConfigServer Services)"
 
 def detect(content, **kwargs):
     detection_schema = (
-        re.compile(r"<img.src(=)?.csf[-_]?small.(jpg|png)?", re.I),
+        re.compile(
+            r"<img.src(=)?(.csf|.)?([-_]|data)"
+            r"?(small)?.(image.)?(jpg|png)?(.)"
+            r"?(base64.)?((?:[A-Za-z0-9+/]{4})"
+            r"*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-"
+            r"9+/]{3}=)?)?", re.I),
+        re.compile(r"<img.src(=)?.csf[-_]?small.(jpg|png)?", re.I)
     )
     for detection in detection_schema:
         if detection.search(content) is not None:
