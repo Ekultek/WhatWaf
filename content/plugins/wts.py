08c594d21fe3ee8d9ae5bf2ab092679a08dda4e8
ekultek <Ekultek@users.noreply.github.com>
2018-08-21 11:16:59 -0500

    creates detection for WTS (issue #104)

---
+++ content/plugins/wts.py
@@ -0,0 +1,15 @@
+# coding=utf-8
+import re
+
+
+__product__ = "WTS-WAF (Web Application Firewall)"
+
+
+def detect(content, **kwargs):
+    content = str(content)
+    detection_schema = (
+        re.compile(r"wts.waf", re.I),
+    )
+    for detection in detection_schema:
+        if detection.search(content) is not None:
+            return True
