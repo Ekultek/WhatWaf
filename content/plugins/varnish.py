f16c7e02f1ee1950500f44e4821371456548f36b
ekultek <Ekultek@users.noreply.github.com>
2018-08-24 16:39:39 -0500

    updated Varnish (issue #105)

--- content/plugins/varnish.py
+++ content/plugins/varnish.py
@@ -3,7 +3,7 @@ import re
 from lib.settings import HTTP_HEADER
 
 
-__product__ = "Varnish FireWall (OWASP)"
+__product__ = "Varnish/CacheWall WAF"
 
 
 def detect(content, **kwargs):
@@ -13,7 +13,8 @@ def detect(content, **kwargs):
         re.compile(r"\bxid. \d+", re.I),
         re.compile(r"varnish\Z", re.I),
         re.compile(r"varnish"), re.I,
-        re.compile(r"\d+")
+        re.compile(r"\d+"),
+        re.compile(r"<.+>(.)?security.by.cachewall(.)?<.+.>", re.I)
     )
     try:
         for detection in detection_schema:
@@ -25,5 +26,8 @@ def detect(content, **kwargs):
                 return True
             if detection.search(headers.get("X-Varnish", "")) is not None:
                 return True
+        possible_headers = ("X-Varnish", "X-Cachewall-Action", "X-Cachewall-Reason")
+        if any([h in k for k in headers.keys() for h in possible_headers]):
+            return True
     except:
         pass
