4a438b779af9fdd144d15786da8498bbf9e2266d
ekultek <Ekultek@users.noreply.github.com>
2018-09-21 09:52:38 -0500

    expiremental branch for install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+DATE=$(date +'%m-%d-%Y %H:%M:%S')
+
 chmod +x whatwaf.py
 mkdir /usr/local/etc/whataf
 cp -pRu ./* /usr/local/etc/whataf
-cat<<EOT >> /usr/local/bin/whatwaf
-#!/bin/bash
-# this is the execution script for whatWaf
-# created by whatwaf install.sh on $(date +'%m-%d-%Y %H:%M:%S')
-cd /usr/local/etc/whataf
-exec python whatwaf.py $@
-EOT
+python -c '
+import os;
+import stat;
+with open("/usr/local/bin/whatwaf","a+") as whatwaf:
+    whatwaf.write("#!/bin/bash\n# this is the execution script for whatwaf\n# created by the whatwaf installer on ${DATE}\ncd /usr/local/etc/whatwaf\nexec python whatwaf.py $@");
+st=os.stat("/usr/local/bin/whatwaf");
+os.chmod("/usr/local/bin/whatwaf",st.st_mode | stat.S_IEXEC)
+'
 chmod +x /usr/local/bin/whatwaf
 pip install -r requirements.txt
