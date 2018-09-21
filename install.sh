892ebc6983e6fe289a00b4915cebc8037ca2eb8a
ekultek <Ekultek@users.noreply.github.com>
2018-09-21 09:30:27 -0500

    expiremental branch for install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-# test commit
-
 chmod +x whatwaf.py
+cp -R ./whatwaf /usr/local/etc/whatwaf
+cat<<EOT >> /usr/local/bin/whatwaf
+#!/bin/bash
+# this is the execution script for whatWaf
+# created by whatwaf install.sh on $(date +'%m-%d-%Y %H:%M:%S')
+exec python /usr/local/etc/whatwaf/whatwaf.py $@
+EOT
+chmod +x /usr/local/bin/whatwaf
 pip install -r requirements.txt
