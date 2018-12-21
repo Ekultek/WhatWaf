de136c5704b5ef82ebe557b4a98656aa20f4f3fc
ekultek <Ekultek@users.noreply.github.com>
2018-12-21 08:46:35 -0600

    fixes detection issue in bigip wafs, issue #177

--- content/plugins/bigip.py
+++ content/plugins/bigip.py
@@ -9,7 +9,7 @@ __product__ = "BIG-IP Application Security Manager (F5 Networks)"
 def detect(content, **kwargs):
     headers = kwargs.get("headers", None)
     detection_schema = (
-        re.compile(r"\ATS\w{4,}=", re.I), re.compile(r"biG]gip|bipserver", re.I),
+        re.compile(r"\ATS\w{4,}=", re.I), re.compile(r"bigip|bipserver|bigipserverinternal", re.I),
         re.compile(r"\AF5\Z", re.I),
         re.compile(r"^TS[a-zA-Z0-9]{3,8}=", re.I)
     )
