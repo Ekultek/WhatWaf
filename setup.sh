b5dc9388db3b2c8546c6c47675cd78de895d4498
ekultek <Ekultek@users.noreply.github.com>
2019-01-10 11:12:20 -0600

    made some updates to the installer, shouldnt need to re-run it

--- setup.sh
+++ setup.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
 
+CURRENT_WHATWAF_VERSION=$(cat ./lib/settings.py | grep 'VERSION = ' | cut -d' ' -f 3 | cut -d'"' -f 2)
+
+
 function helpPage {
   echo -e "\n./setup.sh {install|remove}\n";
+}
+
+function banner {
+    echo -e "	                          ,------.   ";
+    echo -e "	                         '  .--.  '  ";
+    echo -e "	,--.   .--.   ,--.   .--.|  |  |  |  ";
+    echo -e "	|  |   |  |   |  |   |  |'--'  |  |  ";
+    echo -e "	|  |   |  |   |  |   |  |    __.  |  ";
+    echo -e "	|  |.'.|  |   |  |.'.|  |   |   .'   ";
+    echo -e "	|         |   |         |   |___|    ";
+    echo -e "	|   ,'.   |hat|   ,'.   |af .---.    ";
+    echo -e "	'--'   '--'   '--'   '--'   '---'    v($CURRENT_WHATWAF_VERSION)";
 }
 
 function install {
@@ -22,13 +37,16 @@ function install {
 #!/bin/bash
 # this is the execution script for whatwaf
 # created by the whatwaf installer on $(date +%F)
+# this execution script was designed with version $CURRENT_WHATWAF_VERSION
+# installation. If you have any issues with this execution script or this
+# installation please report them here: https://github.com/Ekultek/WhatWaf/issues/new
 cd $copy_dir
 exec python whatwaf.py \$@
 EOF
   echo "editing file stats";
   chmod +x $exec_filename;
   echo "export PATH=\"$PATH:$exec_dir\"" >> $HOME/.bash_profile;
-  echo "installed, you need to run: source ~/.bash_profile";
+  echo "installed, you need to run: source ~/.bash_profile if you notice that the installation does not work as expected";
 }
 
 function uninstall {
@@ -37,29 +55,12 @@ function uninstall {
 }
 
 function main {
+  banner;
   if [[ "$1" == "install" ]]; then
     echo -e " Installing:";
-    echo -e "	                          ,------. ";
-    echo -e "	                         '  .--.  '";
-    echo -e "	,--.   .--.   ,--.   .--.|  |  |  |";
-    echo -e "	|  |   |  |   |  |   |  |'--'  |  |";
-    echo -e "	|  |   |  |   |  |   |  |    __.  |";
-    echo -e "	|  |.'.|  |   |  |.'.|  |   |   .' ";
-    echo -e "	|         |   |         |   |___|  ";
-    echo -e "	|   ,'.   |hat|   ,'.   |af .---.  ";
-    echo -e "	'--'   '--'   '--'   '--'   '---'  ";
     install;
   elif [[ "$1" == "remove" ]]; then
     echo -e " Uninstalling:";
-    echo -e "	                          ,------. ";
-    echo -e "	                         '  .--.  '";
-    echo -e "	,--.   .--.   ,--.   .--.|  |  |  |";
-    echo -e "	|  |   |  |   |  |   |  |'--'  |  |";
-    echo -e "	|  |   |  |   |  |   |  |    __.  |";
-    echo -e "	|  |.'.|  |   |  |.'.|  |   |   .' ";
-    echo -e "	|         |   |         |   |___|  ";
-    echo -e "	|   ,'.   |hat|   ,'.   |af .---.  ";
-    echo -e "	'--'   '--'   '--'   '--'   '---'  ";
     uninstall;
   else
     helpPage;
