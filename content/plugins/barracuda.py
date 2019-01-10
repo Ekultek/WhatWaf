b765223b82caaf1796f4c32594237860511a4479
ekultek <Ekultek@users.noreply.github.com>
2019-01-10 11:00:26 -0600

    update for baraccuda firewall thanks to issue #192

--- content/plugins/barracuda.py
+++ content/plugins/barracuda.py
@@ -3,15 +3,20 @@ import re
 from lib.settings import HTTP_HEADER
 
 
-__product__ = "Barracuda Web Application Firewall (Barracuda Networks)"
+__product__ = "Barracuda Web Application Firewall (Barracuda Networks, Inc.)"
 
 
 def detect(content, **kwargs):
     headers = kwargs.get("headers", None)
+    content = str(content)
+
     detection_schema = (
         re.compile(r"\Abarra.counter.session(=)?", re.I),
-        re.compile(r"(\A|\b)?barracuda.", re.I)
+        re.compile(r"(\A|\b)?barracuda.", re.I),
+        re.compile(r"barracuda.networks(.)?.inc", re.I)
     )
     for detection in detection_schema:
         if detection.search(headers.get(HTTP_HEADER.SET_COOKIE, "")) is not None:
+            return True
+        if detection.search(content) is not None:
             return True
