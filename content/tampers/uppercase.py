87b7f76bf7f9b4699ea0b205de821c033b1a09c1
ziofil <ziofil@users.noreply.github.com>
2019-02-08 22:21:22 +0100

    typo

--- content/tampers/uppercase.py
+++ content/tampers/uppercase.py
@@ -1,5 +1,5 @@
 __example_payload__ = '<script>alert("test");</script>'
-__type__ = "changing the payload into it's uppercase equivalent"
+__type__ = "changing the payload into its uppercase equivalent"
 
 
 def tamper(payload, **kwargs):
