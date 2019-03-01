ce0cdfad80ad5a23149fd8e26d0bf06af492d894
ekultek <Ekultek@users.noreply.github.com>
2019-03-01 16:00:14 -0600

    minor update to aws waf detection for issue #228

--- content/plugins/aws.py
+++ content/plugins/aws.py
@@ -12,7 +12,7 @@ def detect(content, **kwargs):
     detection_schema = (
         re.compile(r"<RequestId>[0-9a-zA-Z]{16,25}<.RequestId>", re.I),
         re.compile(r"<Error><Code>AccessDenied<.Code>", re.I),
-        re.compile(r"\bAWS", re.I),
+        re.compile(r"\baws(alb)?.", re.I),
         re.compile(r"x.amz.id.\d+", re.I),
         re.compile(r"x.amz.request.id", re.I),
         re.compile(r"amazon.\d+", re.I)
@@ -23,4 +23,6 @@ def detect(content, **kwargs):
         if detection.search(headers.get(HTTP_HEADER.SERVER, "")) is not None:
             return True
         if detection.search(headers.get(HTTP_HEADER.X_POWERED_BY, "")) is not None:
+            return True
+        if detection.search(headers.get(HTTP_HEADER.SET_COOKIE, "")) is not None:
             return True
