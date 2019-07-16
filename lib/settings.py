86084a38746fc4190b74f8eac811655815256c29
Ekultek <god_lok1@protonmail.com>
2019-07-16 10:32:22 -0500

    bumps the version number

--- lib/settings.py
+++ lib/settings.py
@@ -23,7 +23,7 @@ import lib.database
 warnings.simplefilter('ignore', InsecureRequestWarning)
 
 # version number <major>.<minor>.<commit>
-VERSION = "1.5.1"
+VERSION = "1.5.2"
 
 # version string
 VERSION_TYPE = "($dev)" if VERSION.count(".") > 1 else "($stable)"
