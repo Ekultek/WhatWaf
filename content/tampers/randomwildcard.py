5f05bd0302910fd30095e9f40697fc4aa133e9af
Ekultek <god_lok1@protonmail.com>
2019-07-16 10:30:23 -0500

    creates a random wildcare tamper that changes characters in either '?' or '*' at random

---
+++ content/tampers/randomwildcard.py
@@ -0,0 +1,22 @@
+import random
+import string
+
+
+__example_payload__ = "/bin/cat /etc/passwd"
+__type__ = "changing characters into a wildcard"
+
+
+def tamper(payload, **kwargs):
+    wildcard = ["*", "?"]
+    safe_chars = string.punctuation
+    retval = ""
+    for char in list(payload):
+        if not any(p == char for p in safe_chars):
+            do_it = random.randint(1, 10) <= 3
+            if do_it:
+                retval += random.choice(wildcard)
+            else:
+                retval += char
+        else:
+            retval += char
+    return retval
