538022413d49e84c9e152baf4c4394a7dfd5bdc0
Ekultek <god_lok1@protonmail.com>
2019-07-31 13:27:50 -0500

    creates a tamper script for StrictHttpFirewall (issue #313)

---
+++ content/plugins/stricthttp.py
@@ -0,0 +1,17 @@
+import re
+
+
+# https://docs.spring.io/spring-security/site/docs/4.2.12.RELEASE/apidocs/org/springframework/security/web/firewall/StrictHttpFirewall.html
+__product__ = "StrictHttpFirewall (WAF)"
+
+
+def detect(content, **kwargs):
+    content = str(content)
+    status = kwargs.get("status", None)
+    detection_schema = (
+        re.compile(r"the.request.was.rejected.because.the.url.contained.a.potentially.malicious.string", re.I),
+    )
+    if status == 500:
+        for detection in detection_schema:
+            if detection.search(content) is not None:
+                return True
