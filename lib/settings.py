7e14c1a20b2e634ad9f60b6562b30e8a3febcff1
Ekultek <god_lok1@protonmail.com>
2019-08-21 11:11:03 -0500

    bumps version number to the newest

--- lib/settings.py
+++ lib/settings.py
@@ -28,7 +28,7 @@ except:
 warnings.simplefilter('ignore', InsecureRequestWarning)
 
 # version number <major>.<minor>.<commit>
-VERSION = "1.5.13"
+VERSION = "1.5.14"
 
 # version string
 VERSION_TYPE = "($dev)" if VERSION.count(".") > 1 else "($stable)"
