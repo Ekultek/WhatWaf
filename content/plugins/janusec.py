f074bdc8d1a5c6c287612e160f655df46c483370
Ekultek <god_lok1@protonmail.com>
2019-08-30 16:36:17 -0500

    minor update for janusec script

--- content/plugins/janusec.py
+++ content/plugins/janusec.py
@@ -8,7 +8,7 @@ def detect(content, **kwargs):
     content = str(content)
     detection_schema = (
         re.compile("janusec", re.I),
-        re.compile("http(s)?\W+(www.)?janusec.(com|net|org)", re.I)
+        re.compile("(http(s)?\W+(www.)?)?janusec.(com|net|org)", re.I)
     )
     for detection in detection_schema:
         if detection.search(content) is not None:
