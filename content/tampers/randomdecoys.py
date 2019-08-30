6d258a8159bcd6fe536ac88d1bc8681e4d8b13f0
Ekultek <god_lok1@protonmail.com>
2019-08-30 16:37:59 -0500

    created a new tamper script for adding random decoy tags to a xss payload

--- content/tampers/randomdecoys.py
+++ content/tampers/randomdecoys.py
@@ -1,4 +1,5 @@
 import re
+import random
 
 
 __example_payload__ = "<script>alert(1);</script>"
@@ -6,5 +7,33 @@ __type__ = "add decoy tags to the script"
 
 
 def tamper(payload, **kwargs):
-    # 
+    retval = ""
+    # https://stackoverflow.com/questions/27044221/regular-expression-to-match-different-script-tags-in-python
     searcher = re.compile(r"(<\s*?script[\s\S]*?(?:(?:src=[\'\"](.*?)[\'\"])(?:[\S\s]*?))?>)([\s\S]*?)(</script>)", re.I)
+    if searcher.search(payload) is None:
+        # we'll just skip payloads that aren't xss
+        return payload
+    decoys = (
+        "<decoy>", "<lillypopper>",
+        "<whatwaf>", "<xanxss>",
+        "<teapot>", "<svg>"
+    )
+    retval += random.choice(decoys)
+    for char in payload:
+        do_it = random.randint(1, 5) < 3
+        if char == "<":
+            if do_it:
+                retval += "{}{}".format(random.choice(decoys), char)
+            else:
+                retval += char
+        elif char == ">":
+            if do_it:
+                retval += "{}{}".format(char, random.choice(decoys))
+            else:
+                retval += char
+        else:
+            retval += char
+    if not retval == payload:
+        return retval
+    else:
+        return tamper(payload, **kwargs)
