1f870a134ccb4605485afd3d9fcede2116429cdf
Ekultek <god_lok1@protonmail.com>
2019-09-17 08:17:05 -0500

    DiDiYun waf script for issue #375

---
+++ content/tampers/didiyun.py
@@ -0,0 +1,23 @@
+import re
+
+from lib.settings import HTTP_HEADER
+
+
+__product__ = "DiDiYun WAF (DiDi)"
+
+
+def detect(content, **kwargs):
+    content = str(content)
+    headers = kwargs.get("headers", None)
+    detection_schema = (
+        re.compile("(http(s)?://)(sec-waf.|www.)?didi(static|yun)?.com(/static/cloudwafstatic)?", re.I),
+        re.compile("didiyun", re.I)
+    )
+    for detection in detection_schema:
+        if detection.search(content) is not None:
+            return True
+    if headers is not None:
+        server = headers.get(HTTP_HEADER.SERVER, None)
+        if server is not None:
+            if server == "DiDi-SLB":
+                return True
