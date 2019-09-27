e4adb3004f1634fde25a8115767e01deb713c415
Ekultek <god_lok1@protonmail.com>
2019-09-27 09:48:56 -0500

    bumps version number

--- lib/settings.py
+++ lib/settings.py
@@ -28,7 +28,7 @@ except:
     pass
 
 # version number <major>.<minor>.<commit>
-VERSION = "1.6.6"
+VERSION = "1.6.7"
 
 # version string
 VERSION_TYPE = "($dev)" if VERSION.count(".") > 1 else "($stable)"
