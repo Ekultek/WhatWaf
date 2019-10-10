00ae5063745c5a8dfb7e730d3ff9e95fa375c1f1
Ekultek <god_lok1@protonmail.com>
2019-10-10 09:02:24 -0500

    fixes issue #475

--- content/plugins/aspgeneric.py
+++ content/plugins/aspgeneric.py
@@ -23,6 +23,7 @@ def detect(content, **kwargs):
     x_powered_by = headers.get(HTTP_HEADER.X_POWERED_BY, "")
     asp_header = headers.get("X-ASPNET-Version", "")
     set_cookie = headers.get(HTTP_HEADER.SET_COOKIE, "")
+    asp_header_2 = headers.get("asp-id", "")
     for detection in detection_schema:
         if detection.search(content) is not None:
             detected += 1
@@ -30,6 +31,8 @@ def detect(content, **kwargs):
             detected += 1
     detected += 1 if asp_header != "" else 0
     if detection_schema[4].search(x_powered_by) is not None:
+        detected += 1
+    if asp_header_2 != "":
         detected += 1
     if detected >= 2:
         return True
