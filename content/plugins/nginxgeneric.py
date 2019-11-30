0c93cc6ed01ea2299dd585e59c5a559510fcd736
Ekultek <ekultek@noreply.github.com>
2019-11-29 22:15:40 -0600

    adding nginx too because why not (issue #528)

---
+++ content/plugins/nginxgeneric.py
@@ -0,0 +1,15 @@
+import re
+
+
+__product__ = "Nginx Gneric Protection"
+
+
+def detect(content, **kwargs):
+    content = str(content)
+    detection_schema = (
+        re.compile("nginx", re.I),
+        re.compile("you.do(not|n.t)?.have.permission.to.access.this.document")
+    )
+    for detection in detection_schema:
+        if detection.search(content) is not None:
+            return True
