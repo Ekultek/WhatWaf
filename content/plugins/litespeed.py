9a8774d3cba0beaceb5fc3e063aa82019eff1cb5
Ekultek <ekultek@noreply.github.com>
2019-11-29 21:42:37 -0600

    adding litespeed as well

---
+++ content/plugins/litespeed.py
@@ -0,0 +1,14 @@
+import re
+
+
+__product__ = "Litespeed webserver Generic Protection"
+
+
+def detect(content, **kwargs):
+    content = str(content)
+    detection_schema = (
+        re.compile("litespeed.web.server", re.I),
+    )
+    for detection in detection_schema:
+        if detection.search(content) is not None:
+            return True
