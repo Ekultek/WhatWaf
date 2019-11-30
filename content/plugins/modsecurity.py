477015b0fe27548bae6e264bedac6d1c2f446198
Ekultek <ekultek@noreply.github.com>
2019-11-29 20:10:46 -0600

    fixes issue #615

--- content/plugins/modsecurity.py
+++ content/plugins/modsecurity.py
@@ -8,7 +8,7 @@ def detect(content, **kwargs):
     content = str(content)
     detection_schema = (
         re.compile(r"ModSecurity|NYOB", re.I),
-        re.compile(r"mod.security", re.I),
+        re.compile(r"mod_security", re.I),
         re.compile(r"This.error.was.generated.by.mod.security", re.I),
         re.compile(r"web.server at", re.I),
         re.compile(r"page.you.are.(accessing|trying)?.(to|is)?.(access)?.(is|to)?.(restricted)?", re.I)
