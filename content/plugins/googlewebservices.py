6b89699636cec1bc015f5ab82354ac509976fd26
Ekultek <ekultek@noreply.github.com>
2019-11-29 20:30:04 -0600

    updated for issue #603

--- content/plugins/googlewebservices.py
+++ content/plugins/googlewebservices.py
@@ -9,8 +9,9 @@ def detect(content, **kwargs):
     content = str(content)
     detection_schema = (
         re.compile("your.client.has.issued.a.malformed.or.illegal.request", re.I),
+        re.compile("our.systems.have.detected.unusual.traffic")
     )
-    if status == 400:
+    if status == 400 or status == 429:
         for detection in detection_schema:
             if detection.search(content) is not None:
                 return True
