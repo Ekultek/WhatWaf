8b2732983dec9209647a5ca57eec607c0dcedac6
Ekultek <ekultek@noreply.github.com>
2019-11-29 19:55:15 -0600

    this should fix some issues with firewall detections

--- content/plugins/unknown.py
+++ content/plugins/unknown.py
@@ -10,10 +10,13 @@ def detect(content, **kwargs):
     content = str(content)
     headers = kwargs.get("headers", {})
     status = kwargs.get("status", 0)
+    discovered = 0
     # make sure that it's not just a `didn't find what you're looking for` page
     # this will probably help out a lot with random WAF detection
     if status == 200 or "not found" in content.lower():
         return False
+    if status == 404:
+        discovered -= 1
     detection_schema = (
         re.compile(r"blocked", re.I), re.compile(r"illegal", re.I),
         re.compile(r"reported", re.I), re.compile(r"ip.logged", re.I),
@@ -27,13 +30,15 @@ def detect(content, **kwargs):
     )
     for detection in detection_schema:
         if detection.search(content) is not None:
-            return True
+            discorvered += 1
         for head in headers.keys():
             if detection.search(headers[head]) is not None:
-                return True
+                discovered += 1
             if detection.search(head) is not None:
                 if not any(head == c for c in [HTTP_HEADER.CONTENT_SECURITY, HTTP_HEADER.STRICT_TRANSPORT]):
-                    return True
+                    discovered += 1
         unknown_verification_regex = re.compile(r"(\b)?<.+>*({})(\S+|\d+|\w+)?(<.+.>)?".format(detection.pattern), re.I)
         if unknown_verification_regex.search(content) is not None:
-            return True
+            discovered += 1
+    if discovered > 2:
+        return True
