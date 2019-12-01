abebb70d707df9da71c63c536607d9b8fe608cd1
Ekultek <ekultek@noreply.github.com>
2019-12-01 17:17:50 -0600

    stackpath waf added (issue #516)

---
+++ content/plugins/stackpath.py
@@ -0,0 +1,15 @@
+import re
+
+
+__product__ = "Stackpath WAF (StackPath)"
+
+
+def detect(content, **kwargs):
+    content = str(content)
+    detection_schema = (
+        re.compile("action.that.triggered.the.service.and.blocked", re.I),
+        re.compile("<h2>sorry,.you.have.been.blocked(.)?<.h2>", re.I)
+    )
+    for detection in detection_schema:
+        if detection.search(content) is not None:
+            return True
