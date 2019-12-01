0f55100ed17aa0e36a46a9554a99daf0d3aad67f
Ekultek <ekultek@noreply.github.com>
2019-12-01 16:53:37 -0600

    update to WTS for issue #573

--- content/plugins/wts.py
+++ content/plugins/wts.py
@@ -7,7 +7,7 @@ __product__ = "WTS-WAF (Web Application Firewall)"
 def detect(content, **kwargs):
     content = str(content)
     detection_schema = (
-        re.compile(r"wts.waf(\w+)?", re.I),
+        re.compile(r"(<title>)?wts.wa(f)?(\w+(\w+(\w+)?)?)?", re.I),
     )
     for detection in detection_schema:
         if detection.search(content) is not None:
