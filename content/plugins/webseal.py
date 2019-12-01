c41038ad495d3b70cbaf538041a3f0a1d04a3f2d
Ekultek <ekultek@noreply.github.com>
2019-12-01 17:35:57 -0600

    update to webseal for issue #495

--- content/plugins/webseal.py
+++ content/plugins/webseal.py
@@ -1,14 +1,20 @@
 import re
 
+from lib.settings import HTTP_HEADER
+
 
 __product__ = "IBM Security Access Manager (WebSEAL)"
 
 
 def detect(content, **kwargs):
     content = str(content)
+    headers = kwargs.get("headers", {})
     detection_schema = (
-        re.compile(r"\bWebSEAL\b", re.I),
+        re.compile("webseal.error.message.template", re.I),
+        re.compile("webseal.server.received.an.invalid.http.request", re.I)
     )
+    if headers.get(HTTP_HEADER.SERVER, "") == "WebSEAL":
+        return True
     for detection in list(detection_schema):
         if detection.search(content) is not None:
             return True
