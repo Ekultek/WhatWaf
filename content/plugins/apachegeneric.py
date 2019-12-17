4b3f9a63be9c883e365db26a5c0bc696f7246d66
Ekultek <ekultek@noreply.github.com>
2019-12-17 11:12:32 -0600

    patch for apache generic detection (issue #656)

--- content/plugins/apachegeneric.py
+++ content/plugins/apachegeneric.py
@@ -1,5 +1,7 @@
 import re
 
+from lib.settings import HTTP_HEADER
+
 
 __product__ = "Apache Generic"
 
@@ -7,13 +9,16 @@ __product__ = "Apache Generic"
 def detect(content, **kwargs):
     content = str(content)
     status = kwargs.get("status", 0)
+    headers = kwargs.get("headers", {})
     detection_schema = (
         re.compile("apache", re.I),
-        re.compile("You.don.t.have.permission.to.access", re.I),
-        re.compile("was.not.found.on.this.server", re.I)
+        re.compile("you.don.t.have.permission.to.access.", re.I),
+        re.compile("was.not.found.on.this.server", re.I),
+        re.compile("<address>apache/([\d+{1,2}](.[\d+]{1,2}(.[\d+]{1,3})?)?)?", re.I)
     )
-
     if status == 403:
+        if detection_schema[0].search(headers.get(HTTP_HEADER.SERVER, "")) is not None:
+            return True
         for detection in detection_schema:
             if detection.search(content) is not None:
                 return True
