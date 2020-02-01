9f96fbd355ea2241573ea7f8bf64e970f5cbf3b4
Ekultek <ekultek@noreply.github.com>
2020-01-31 19:34:24 -0600

    another update for modsec issue #665

--- content/plugins/modsecurity.py
+++ content/plugins/modsecurity.py
@@ -9,7 +9,7 @@ def detect(content, **kwargs):
     detection_schema = (
         re.compile(r"ModSecurity|NYOB", re.I),
         re.compile(r"mod_security", re.I),
-        re.compile(r"This.error.was.generated.by.mod.security", re.I),
+        re.compile(r"this.error.was.generated.by.mod.security", re.I),
         re.compile(r"web.server at", re.I),
         re.compile(r"page.you.are.(accessing|trying)?.(to|is)?.(access)?.(is|to)?.(restricted)?", re.I),
         re.compile(r"blocked.by.mod.security", re.I)
--- lib/settings.py
+++ lib/settings.py
@@ -28,7 +28,7 @@ except:
     pass
 
 # version number <major>.<minor>.<commit>
-VERSION = "1.9.16"
+VERSION = "1.9.17"
 
 # version string
 VERSION_TYPE = "($dev)" if VERSION.count(".") > 1 else "($stable)"
